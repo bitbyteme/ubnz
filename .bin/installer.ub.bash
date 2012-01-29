cf56331686228eecdd650cd616fc39dc9495f96b
bitbyteme <bitbyteme@gmail.com>
2012-01-29 10:47:21 -0800

    Darwin bits-MacBook-Pro.local 11.2.0 at 2012.01.29 10:47:21 -0800

--- .bin/installer.ub.bash
+++ .bin/installer.ub.bash
@@ -34,10 +34,10 @@ fn_setup_gogrid02(){
    #echo 'export phase=02' >> ~/.bashrc
    #curl 'https://raw.github.com/bitbyteme/ubnz/master/.bin/all.pkgs.min' > "$tmp/all.pkgs.min" || exit $err
 
-   #dpkg-query -W -f='${package}\n' > "$tmp/all.pkgs.gogrid"  
-   #cat "$tmp/all.pkgs.gogrid" | while read pp; do 
-   #   grep -q "$pp" "$tmp/all.pkgs.min" || echo "$pp" >> "$tmp/extra" 
-   #done
+   dpkg-query -W -f='${package}\n' > "$tmp/all.pkgs.gogrid"  
+   cat "$tmp/all.pkgs.gogrid" | while read pp; do 
+      grep -q "$pp" "$tmp/all.pkgs.min" || echo "$pp" >> "$tmp/extra" 
+   done
 
    # assuming the new linux kernel installed is the updated one.
    # removing all pkgs different in the ub.gogrid from vmware version.
