f9588f95611dace0763b4d8f47491bcaf64245e2
bitbyteme <bitbyteme@gmail.com>
2012-01-30 14:18:12 -0800

    Darwin bits-MacBook-Pro.local 11.2.0 at 2012.01.30 14:18:12 -0800

--- .bin/installer.ub.bash
+++ .bin/installer.ub.bash
@@ -150,7 +150,7 @@ fn_setup_sys(){
    err=11
    mkdir ~/.backups 2>/dev/null
    mv "$HOME/$gitRepo/.dotfiles" "$HOME/." &&
-   mv ~/.bashrc  ~/.vimrc ~/.vim ~/.zshrc ~/.backups/. || exit $err
+   mv ~/.bashrc  ~/.vimrc ~/.vim ~/.zshrc ~/.backups/. 2>/dev/null
    
    (ln -s  ~/.dotfiles/bashrc ~/.bashrc
    ln -s ~/.dotfiles/zshrc ~/.zshrc
